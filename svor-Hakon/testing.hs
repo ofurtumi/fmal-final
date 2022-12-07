b347787a20b0715d9be87eb0b46e44cd7af14d43
hakoningir <hir12@hi.is>
2022-12-07 19:33:55 +0000

    pusha mörgu margt vitlaust, lítið rétt

--- svor-Hakon/testing.hs
+++ svor-Hakon/testing.hs
@@ -1,9 +1,25 @@
+{-
+Notkun: foldleft f u x
+Fyrir:  f er tvíundarfall,
+        u er heiltala,
+        x er listi af heiltölum
+Eftir:  (((f u x1) f x2)f .. )f xN
+-}
 
 foldleft f u [] = u
 foldleft f u (x:xs) = foldleft f (f u x) xs
-
+{-
+Notkun: foldright f x u
+Fyrir:  f er tvíundarfall
+        x er listi
+        u er tala
+Eftir:  f x1(f x2(f .. (f xN u)))
+-}
 foldright f [] u = u
 foldright f (x:xs) u = foldright f xs (f x u)
+
+atoi i n = [n, n-1 .. i]
+itoa i n = [i .. n]
 
 main :: IO()
 main = do
@@ -13,3 +29,7 @@ main = do
     print("---------------------------")
     print(foldright (\x y -> x - y) x 2)
     print(foldr (\x y -> x - y) 2 x)
+    print("---------------------------")
+    print(atoi 1 5)
+    print(itoa 1 5)
+    print("---------------------------")
